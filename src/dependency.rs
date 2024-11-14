c7ab9811e0055b2bafdfdebf85f42f9f5fae6d8c
Josh Jeppson <joshua.jeppson@usu.edu>
2024-11-14 13:55:56 -0700

    yoink

--- src/dependency.rs
+++ src/dependency.rs
@@ -1,5 +1,39 @@
+use prusti_contracts::*;
 
-#[derive(Debug)]
+struct Transition<'a> {
+    increment_vector: &'a [u64],
+	decrement_vector: &'a [u64],
+	transition_name: String,
+
+}
+
 struct DependencyGraph {
+
+}
+
+struct GraphNode<'a> {
+	transition: Box<Transition<'a>>,
+	children: Vec<Box<GraphNode<'a>>>,
+}
+
+impl<'a> GraphNode<'a> {
+	// #[pure]
+	#[requires(state.len() == self.transition.increment_vector.len())]
+	#[requires(state.len() == self.transition.decrement_vector.len())]
+	#[ensures(result <==>
+			  //forall(|i: usize| {
+		//i < state.len() ==> (state[i] >= self.transition.decrement_vector[i])
+	}))]
+	fn is_enabled(&self, state: &'a [u64]) -> bool {
+		(0..state.len()).try_fold(true, |_acc, i| {
+			body_invariant!(i < state.len());
+			if state[i] >= self.transition.decrement_vector[i] {
+				Some(true)
+			}
+			else {
+				None
+			}
+		}).is_some()
+	}
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
-use clap::*;
+mod dependency;
 
-#[derive(Debug, Clone)]
-enum Method {
-    Stamina,
-    Wayfarer,
-	Ragtimer,
-}
-
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
-struct Args {
-	/// Filename
-	#[arg(short, long)]
-	filename : String,
-
-	// Method of analysis to use
-	// #[arg(short, long)]
-	//method : Method,
-}
+// use clap::*;
+// use prusti_contracts::extern_spec;
+// 
+// #[derive(Debug, Clone)]
+// enum Method {
+//     Stamina,
+//     Wayfarer,
+// 	Ragtimer,
+// }
+// 
+// #[derive(Debug, Parser)]
+// #[command(version, about, long_about = None)]
+// struct Args {
+// 	/// Filename
+// 	#[arg(short, long)]
+// 	filename : String,
+// 
+// 	// Method of analysis to use
+// 	// #[arg(short, long)]
+// 	//method : Method,
+// }
+// 
+// #[extern_spec]
+// impl Parser {
+// 	#[trusted]
+// }
+// 
+// fn main() {
+// 	let args = Args::parse();
+// 	println!("Got filename {}", args.filename);
+// }
 
 fn main() {
-	let args = Args::parse();
-	println!("Got filename {}", args.filename);
+	let _x = 5;
 }
