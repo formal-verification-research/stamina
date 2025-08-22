601a4e9a0981e0cde84fedd49f8f8a8187f2f6bf
Landon Taylor <landon@mossbiscuits.com>
2025-08-22 11:06:27 -0600

    retry trace attempts

--- src/builder/ragtimer/rl_traces.rs
+++ src/builder/ragtimer/rl_traces.rs
@@ -495,15 +495,15 @@ impl<'a> RagtimerBuilder<'a> {
 			let mut trace_probability;
 			let mut trace_attempts = 0;
 			loop {
-				trace_attempts += 1;
-				if trace_attempts > 100 {
-					warning!("Exceeded maximum attempts to generate a unique trace (100 attempts). Moving on to next trace.");
-					break;
-				}
 				// Generate a single trace
 				(trace, trace_probability) = self.generate_single_trace(&rewards);
 				// If the trace already exists or is empty, we try to generate a new one.
 				if !trace_trie.exists_or_insert(&trace) && !trace.is_empty() {
+					break;
+				}
+				trace_attempts += 1;
+				if trace_attempts > 100 {
+					warning!("Exceeded maximum attempts to generate a unique trace (100 attempts). Moving on to next trace.");
 					break;
 				}
 				debug_message!("Trace {} already exists, generating a new one (attempt {}/100).", i, trace_attempts);
